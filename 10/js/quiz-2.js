9f6a0c47735468bca041751349a8f4b30ce50e88
jeong-yeji <jeongyeji.mail@gmail.com>
2022-04-04 16:07:58 +0900

    10. 브라우저 객체 모델(BOM; Browser Object Model)

---
+++ 10/js/quiz-2.js
@@ -0,0 +1,4 @@
+const r = Math.floor(Math.random() * 256 + 1);
+const g = Math.floor(Math.random() * 256 + 1);
+const b = Math.floor(Math.random() * 256 + 1);
+document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
